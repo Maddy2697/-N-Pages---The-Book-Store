e5d87e8f323cb43e4a2e771fc47c23fda005c4ba
Mudassar <99661023+Maddy2697@users.noreply.github.com>
2023-04-13 22:19:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    "./resources/**/*.{vue, blade.php, js, jsx, ts, tsx}"
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+
+    
+
+  ],
+}
+
